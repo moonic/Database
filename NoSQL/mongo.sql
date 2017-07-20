841682f7b50faad273b169770bad6b50c07aff5b
moonic <1160708553@qq.com>
2017-07-20 23:06:52 +0800

    Update mongo.sql

--- NoSQL/mongo.sql
+++ NoSQL/mongo.sql
@@ -48,23 +48,40 @@ I 内嵌文档 key: {"document"}
 ------------------------------
 
 ---------- 插入数据------------
-db.persion.save({_id: 1,name: "jock",age: 20,date: Date(), elam: "jack@cure.com"})
-db.persion.save({_id: 2,name: "tom",age: 15,date:  Date(), elam: "tom@cure.com"})
-db.persion.save({_id: 3,name: "tiem",age: 25,date: Date(), elam: "tiem@cure.com"})
-db.persion.save({_id: 4,name: "lose",age: 40,date: Date(), elam: [tiem@cure.com ,tiem1@cure.com,tiem2@cure.com]})
 
-db.book.save({ "_id" : 1, "friends" : 32, "enemies" : 2, "relationships" : { "friends" : 32, " enemies" : 2 }, "username" : "joe" })
-db.book.save({ "_id" : 2, "apple" : 1, "banan" : 6, "peach" : 3 })
-db.book.save({ "_id" : 3, "apple" : 8, "spinach" : 4, "water" : 4 })
+{
+   insert: <collection>,
+   documents: [ <document>, <document>, <document>, ... ],
+   ordered: <boolean>,
+   writeConcern: { <write concern> },
+   bypassDocumentValidation: <boolean>
+}
+db.persion.insert({_id: 1,name: "jock",age: 20,date: Date(), elam: "jack@cure.com"})
+db.persion.insert({_id: 2,name: "tom",age: 15,date:  Date(), elam: "tom@cure.com"})
+db.persion.insert({_id: 3,name: "tiem",age: 25,date: Date(), elam: "tiem@cure.com"})
+db.persion.insert({_id: 4,name: "lose",age: 40,date: Date(), elam: [tiem@cure.com ,tiem1@cure.com,tiem2@cure.com]})
 
-db.blog.save({ "_id" : 1, "title" : "blog post", "content": "nice", "authors" : [ "richie" ] })
-db.blog.save({ "_id" : 2, "title" : "score", "comments" :[ { "name" : "job" }, { "name" : "job" }, { "name" : "job", "age" : 25, "conten"
+db.book.insert({ "_id" : 1, "friends" : 32, "enemies" : 2, "relationships" : { "friends" : 32, " enemies" : 2 }, "username" : "joe" })
+db.book.insert({ "_id" : 2, "apple" : 1, "banan" : 6, "peach" : 3 })
+db.book.insert({ "_id" : 3, "apple" : 8, "spinach" : 4, "water" : 4 })
+
+db.blog.insert({ "_id" : 1, "title" : "blog post", "content": "nice", "authors" : [ "richie" ] })
+db.blog.insert({ "_id" : 2, "title" : "score", "comments" :[ { "name" : "job" }, { "name" : "job" }, { "name" : "job", "age" : 25, "conten"
  : "nice post" } ] })
-db.blog.save({ "_id" : 3, "name" : "jack", "emails" : [ "joe@2.com", "jse@2.com", "jse@3.com", "jon@php.com" ] })
+db.blog.insert({ "_id" : 3, "name" : "jack", "emails" : [ "joe@2.com", "jse@2.com", "jse@3.com", "jon@php.com" ] })
 
-db.nums.save({ "_id" : 1, "myInteger" : { "floatApprox" :3 } })
-db.nums.save({ "_id" : 2, "things" : [ "pie", 3.14 ] })
-db.nums.save({ "_id" :3, "name" : "Jon Doe", "address" :{ "street" : "123 Park Street", "city" : "Anytown", "state" : "NY" } })
+db.nums.insert({ "_id" : 1, "myInteger" : { "floatApprox" :3 } })
+db.nums.insert({ "_id" : 2, "things" : [ "pie", 3.14 ] })
+db.nums.insert({ "_id" :3, "name" : "Jon Doe", "address" :{ "street" : "123 Park Street", "city" : "Anytown", "state" : "NY" } })
+db.stores.insert(
+   [
+     { _id: 1, name: "Java Hut", description: "Coffee and cakes" },
+     { _id: 2, name: "Burger Buns", description: "Gourmet hamburgers" },
+     { _id: 3, name: "Coffee Shop", description: "Just coffee" },
+     { _id: 4, name: "Clothes Clothes Clothes", description: "Discount clothing" },
+     { _id: 5, name: "Java Shopping", description: "Indonesian goods" }
+   ]
+)
 
 > db.products.find()
 { "_id" : 2, "item" : "Unknown", "price" : NumberLong(0) }
@@ -82,9 +99,41 @@ db.collection.insertMany():向指定集合中插入多条文档数据
 # 插入的时候指定id 的编号 不然就自动生成id码无法操作
 --------------------------------
 
+----------- 查询 find------------------------
+{
+   "find": <string>,
+   "filter": <document>,
+   "sort": <document>,
+   "projection": <document>,
+   "hint": <document or string>,
+   "skip": <int>,
+   "limit": <int>,
+   "batchSize": <int>,
+   "singleBatch": <bool>,
+   "comment": <string>,
+   "maxScan": <int>,
+   "maxTimeMS": <int>,
+   "readConcern": <document>,
+   "max": <document>,
+   "min": <document>,
+   "returnKey": <bool>,
+   "showRecordId": <bool>,
+   "snapshot": <bool>,
+   "tailable": <bool>,
+   "oplogReplay": <bool>,
+   "noCursorTimeout": <bool>,
+   "awaitData": <bool>,
+   "allowPartialResults": <bool>,
+   "collation": <document>
+}
+db.collection_name.find() -- 查询集合所有字段
+db.persion.findOne()     --第一个
+db.persion.find({key: value}) -- 对应键值
+db.persion.find().limt()       -- 前多少个
+db.persion.find().skip() 
+db.persion.find().sort()
 
-
-
+-----------------------------------
 
 
 
