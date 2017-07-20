13bbda1e23093d7ab89b8506a727274d85f40d95
moonic <1160708553@qq.com>
2017-07-20 16:00:12 +0800

    Update mongo.sql

--- NoSQL/mongo.sql
+++ NoSQL/mongo.sql
@@ -48,10 +48,29 @@ I 内嵌文档 key: {"document"}
 ------------------------------
 
 ---------- 插入数据------------
-db.persion.save({_id: 1,name: "jock",age: 20,date: "Date()", elam: "jack@cure.com"})
-db.persion.save({_id: 2,name: "tom",age: 15,date: "Date()", elam: "tom@cure.com"})
-db.persion.save({_id: 3,name: "tiem",age: 25,date: "Date()", elam: "tiem@cure.com"})
-db.persion.save({_id: 4,name: "lose",age: 40,date: "Date()", elam: [tiem@cure.com ,tiem1@cure.com,tiem2@cure.com]})
+db.persion.save({_id: 1,name: "jock",age: 20,date: Date(), elam: "jack@cure.com"})
+db.persion.save({_id: 2,name: "tom",age: 15,date:  Date(), elam: "tom@cure.com"})
+db.persion.save({_id: 3,name: "tiem",age: 25,date: Date(), elam: "tiem@cure.com"})
+db.persion.save({_id: 4,name: "lose",age: 40,date: Date(), elam: [tiem@cure.com ,tiem1@cure.com,tiem2@cure.com]})
+
+db.book.save({ "_id" : 1, "friends" : 32, "enemies" : 2, "relationships" : { "friends" : 32, " enemies" : 2 }, "username" : "joe" })
+db.book.save({ "_id" : 2, "apple" : 1, "banan" : 6, "peach" : 3 })
+db.book.save({ "_id" : 3, "apple" : 8, "spinach" : 4, "water" : 4 })
+
+db.blog.save({ "_id" : 1, "title" : "blog post", "content": "nice", "authors" : [ "richie" ] })
+db.blog.save({ "_id" : 2, "title" : "score", "comments" :[ { "name" : "job" }, { "name" : "job" }, { "name" : "job", "age" : 25, "conten"
+ : "nice post" } ] })
+db.blog.save({ "_id" : 3, "name" : "jack", "emails" : [ "joe@2.com", "jse@2.com", "jse@3.com", "jon@php.com" ] })
+
+db.nums.save({ "_id" : 1, "myInteger" : { "floatApprox" :3 } })
+db.nums.save({ "_id" : 2, "things" : [ "pie", 3.14 ] })
+db.nums.save({ "_id" :3, "name" : "Jon Doe", "address" :{ "street" : "123 Park Street", "city" : "Anytown", "state" : "NY" } })
+
+> db.products.find()
+{ "_id" : 2, "item" : "Unknown", "price" : NumberLong(0) }
+{ "_id" : 3, "item" : "XYZ", "price" : NumberLong(50) }
+{ "_id" : 1, "item" : "apple", "price" : 13.7375, "defaultQty" : 100 }
+
 db.collection.insertOne():向指定集合中插入一条文档数据
 db.collection.insertMany():向指定集合中插入多条文档数据
 # 插入的时候指定id 的编号 不然就自动生成id码无法操作
